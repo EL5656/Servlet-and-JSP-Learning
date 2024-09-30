5679137aa52bc84d1c728b2a90ec040f9491f776
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-10-01 01:05:18 +0800

    RequestDispatcher

--- src/main/java/org/practice/AddServlet.java
+++ src/main/java/org/practice/AddServlet.java
@@ -12,13 +12,13 @@ import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
     //service method
-//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        int i = Integer.parseInt(request.getParameter("num1"));//name in index.html
-//        int j = Integer.parseInt(request.getParameter("num2"));
-//        int k = i+j;
-//        PrintWriter out = response.getWriter();
-//        out.println("The result is "+k);
-//    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int i = Integer.parseInt(request.getParameter("num1"));//name in index.html
+        int j = Integer.parseInt(request.getParameter("num2"));
+        int k = i+j;
+        PrintWriter out = response.getWriter();
+        out.println("The result is "+k);
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         int i = Integer.parseInt(request.getParameter("num1"));//name in index.html
